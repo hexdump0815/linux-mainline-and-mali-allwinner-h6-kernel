1e39ad1778a904fa8dad9fb5e51d55e85806e623
hexdump <hexdump0815@googlemail.com>
2020-04-16 19:37:54 +0200

    disable sysrq key by default

--- misc.ah3/options/enable-additional-options.sh
+++ misc.ah3/options/enable-additional-options.sh
@@ -4,6 +4,7 @@ cd /compile/source/linux-stable
 
 ./scripts/config -d CONFIG_EXT2_FS
 ./scripts/config -d CONFIG_EXT3_FS
+./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0
 
 for i in `cat /compile/doc/stable-aw/misc.ah3/options/additional-options-yes.txt`; do
   echo $i
