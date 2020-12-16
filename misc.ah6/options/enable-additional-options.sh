a09e5d1527a37adbc391c3bf23139ea467017866
hexdump0815 <hexdump0815@googlemail.com>
2020-12-16 19:45:32 +0100

    sync across different setups

--- misc.ah6/options/enable-additional-options.sh
+++ misc.ah6/options/enable-additional-options.sh
@@ -2,8 +2,14 @@
 
 cd /compile/source/linux-stable-aw
 
+./scripts/config -d CONFIG_DEBUG_INFO
 ./scripts/config -d CONFIG_EXT2_FS
 ./scripts/config -d CONFIG_EXT3_FS
+./scripts/config -d CONFIG_BLK_DEV_RAM
+./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
+./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
+./scripts/config -d CONFIG_DRM_TEGRA
+./scripts/config -d CONFIG_DRM_NOUVEAU
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0
 
 for i in `cat /compile/doc/stable-aw/misc.ah6/options/additional-options-yes.txt`; do
