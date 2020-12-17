dcd7b4b9bb24bead5d5b62f5dbbf488de578dc83
hexdump0815 <hexdump0815@googlemail.com>
2020-12-17 19:29:38 +0100

    more kernel config cleanup and sync

--- misc.ah6/options/enable-additional-options.sh
+++ misc.ah6/options/enable-additional-options.sh
@@ -3,21 +3,29 @@
 cd /compile/source/linux-stable-aw
 
 ./scripts/config -d CONFIG_DEBUG_INFO
+./scripts/config -d CONFIG_RUNTIME_TESTING_MENU
+./scripts/config -d CONFIG_TEST_KMOD
 ./scripts/config -d CONFIG_EXT2_FS
 ./scripts/config -d CONFIG_EXT3_FS
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
+./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0x0
 ./scripts/config -d CONFIG_DRM_TEGRA
 ./scripts/config -d CONFIG_DRM_NOUVEAU
-./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0
+./scripts/config -d CONFIG_DRM_RADEON
+./scripts/config -d CONFIG_DRM_AMDGPU
+./scripts/config -d CONFIG_DRM_ETNAVIV
+./scripts/config -d CONFIG_XFS_FS
+./scripts/config -d CONFIG_REISERFS_FS
+./scripts/config -d CONFIG_OCFS2_FS
 
-for i in `cat /compile/doc/stable-aw/misc.ah6/options/additional-options-yes.txt`; do
+for i in `cat /compile/doc/stable-aw/misc.ah6/options/additional-options-*-yes.txt`; do
   echo $i
   ./scripts/config -e $i
 done
 
-for i in `cat /compile/doc/stable-aw/misc.ah6/options/additional-options-mod.txt`; do
+for i in `cat /compile/doc/stable-aw/misc.ah6/options/additional-options-*-mod.txt`; do
   echo $i
   ./scripts/config -m $i
 done
